fe781a47761e889cd8d465a26959c9a7d1b224ca
Aaron <aaronclaes22@gmail.com>
2022-05-09 11:00:57 +0200

    feat: finish sidebar user

--- educo/resources/views/layouts/sidebar.blade.php
+++ educo/resources/views/layouts/sidebar.blade.php
@@ -1,51 +1,71 @@
 <div class="flex relative">
-    <div class="h-screen w-72 flex bg-primary">
+    <div class="h-screen w-28 flex bg-primary">
         <nav class="flex flex-col flex-grow overflow-hidden">
-            <div class="flex gap-4 flex-col items-center justify-center mx-4 m-5">
-                <image class="rounded-full h-32 w-32 object-cover" src="https://picsum.photos/id/237/200/300" alt="user-image">
-                    <h3 class="text-white text-2xl font-semibold">Naam</h3>
+            <div class=" mx-5 flex justify-center flex-col my-6">
+                <x-svg.navicons.logo-word />
+            </div>
+            <div class="flex gap-4 flex-col items-center justify-center mx-4 m-5 my-12">
+                <image class="rounded-full h-16 w-16 object-cover" src="https://picsum.photos/id/237/200/300" alt="user-image">
             </div>
             <div class="flex-auto overflow-hidden">
                 <ul class="mx-5">
                     <li class="flex justify-center flex-col my-3">
                         <a href="/dashboard">
                             <div class="w-full flex items-center p-3 rounded-2xl">
-                                <h4 class="ml-4 text-white text-xl font-semibold">Home</h4>
+                                <x-svg.navicons.logo />
                             </div>
                         </a>
                     </li>
                     <li class="flex justify-center flex-col my-3">
                         <a href="/my-courses">
                             <div class="w-full flex items-center p-3 rounded-2xl">
-                                <h4 class="ml-4 text-white text-xl font-semibold">My courses</h4>
+                                <x-svg.navicons.layout class="stroke-white" />
                             </div>
                         </a>
                     </li>
                     <li class="flex justify-center flex-col my-3">
                         <a href="/new-courses">
                             <div class="w-full flex items-center p-3 rounded-2xl">
-                                <h4 class="ml-4 text-white text-xl font-semibold">New courses</h4>
+                                <x-svg.navicons.layout-add class="stroke-white" />
                             </div>
                         </a>
                     </li>
                     <li class="flex justify-center flex-col my-3">
                         <a href="/experts">
                             <div class="w-full flex items-center p-3 rounded-2xl">
-                                <h4 class="ml-4 text-white text-xl font-semibold">Experts</h4>
+                                <x-svg.navicons.teach class="stroke-white" />
                             </div>
                         </a>
                     </li>
                     <li class="flex justify-center flex-col my-3">
                         <a href="/competention-profile">
                             <div class="w-full flex items-center p-3 rounded-2xl">
-                                <h4 class="ml-4 text-white text-xl font-semibold">Competention profile</h4>
+                                <x-svg.navicons.radar class="stroke-white" />
                             </div>
                         </a>
                     </li>
                     <li class="flex justify-center flex-col my-3">
                         <a href="/certificates">
                             <div class="w-full flex items-center p-3 rounded-2xl">
-                                <h4 class="ml-4 text-white text-xl font-semibold">Certificats</h4>
+                                <x-svg.navicons.certificate class="stroke-white" />
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <ul class="mx-5">
+                    <li class="flex justify-center flex-col my-3">
+                        <a href="/dashboard">
+                            <div class="w-full flex items-center p-3 rounded-2xl">
+                                <x-svg.navicons.settings class="stroke-white" />
+                            </div>
+                        </a>
+                    </li>
+                    <li class="flex justify-center flex-col my-3">
+                        <a href="/my-courses">
+                            <div class="w-full flex items-center p-3 rounded-2xl">
+                                <x-svg.navicons.logout class="stroke-white" />
                             </div>
                         </a>
                     </li>
