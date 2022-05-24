9fd9d4e459c0e023c29bea8d1940bac6c216271c
Ruben Rooms <ruben.rooms@endare.com>
2022-05-24 17:42:09 +0200

    Added nav/filter for different course pages

--- educo/resources/views/pages/user/dashboard.blade.php
+++ educo/resources/views/pages/user/dashboard.blade.php
@@ -14,11 +14,15 @@
             </div>
         </div>
     </div>
-
-    <div class="py-5 max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-row">
-        @foreach($courses as $course)
-            <x-Course :course="$course" class="basis-1/3"/>
-        @endforeach
-    </div>
-
+        <div class="py-3 max-w-3xl mx-auto flex flex-row justify-around">
+            <a href="/dashboard"><p>All courses</p></a>
+            <a href="/dashboard/active"><p>Active</p></a>
+            <a href="/dashboard/obligated"><p>Obligated</p></a>
+            <a href="/dashboard/finished"><p>Finished</p></a>
+        </div>
+        <div class="py-5 max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-row">
+            @foreach($courses as $course)
+                <x-Course :course="$course" class="basis-1/3"/>
+            @endforeach
+        </div>
 </x-app-layout>
