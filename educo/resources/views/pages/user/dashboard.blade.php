d13c9e473228550f4a2daafc2738b35fa829b0fb
victormassaer <victor@massaer.be>
2022-05-31 15:59:19 +0200

    code cleanup on user dashboards

--- educo/resources/views/pages/user/dashboard.blade.php
+++ educo/resources/views/pages/user/dashboard.blade.php
@@ -1,9 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Course dashboard') }}
-        </h2>
-    </x-slot>
 
         <div class="py-3 max-w-3xl mx-auto flex flex-row justify-around">
             <a href="/dashboard"><p class="font-semibold">All courses</p></a>
