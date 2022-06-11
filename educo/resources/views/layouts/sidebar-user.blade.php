1272846bc6c62da13ffa590f0d6977f243a715da
Aaron <aaronclaes22@gmail.com>
2022-06-11 18:39:14 +0200

    fix: fix sidebar user

--- educo/resources/views/layouts/sidebar-user.blade.php
+++ educo/resources/views/layouts/sidebar-user.blade.php
@@ -4,52 +4,53 @@
             <div class=" mx-auto flex justify-center flex-col my-6">
                 <x-svg.navicons.logo-word />
             </div>
-            <div class="flex gap-4 flex-col items-center justify-center mx-4 m-5 my-12">
-                <a href="{{route('user.detail.index')}}">
-                    <img class="rounded-full h-16 w-16 object-cover" src="https://picsum.photos/id/237/200/300" alt="user-image">
+            <div class="flex gap-4 flex-col items-center justify-center mx-4 m-5 my-6">
+                <a href="{{ route('user.detail.index') }}">
+                    <img class="rounded-full h-16 w-16 object-cover" src="https://picsum.photos/id/237/200/300"
+                        alt="user-image">
                 </a>
 
             </div>
-            <div class="flex-auto overflow-hidden">
-                <ul class="mx-5">
-                    <li class="flex justify-center flex-col my-3">
+            <div class="flex-auto overflow-auto no-scrollbar" style>
+                <ul class="mx-2">
+                    <li class="flex justify-center flex-col ">
                         <a href="/dashboard">
-                            <div class="w-full flex items-center p-3 rounded-2xl">
+                            <div class="w-10 flex items-center px-2 py-1 rounded-2xl mx-auto">
                                 <x-svg.navicons.logo />
                             </div>
                         </a>
                     </li>
-                    <li class="flex justify-center flex-col my-3">
+                    <li class="flex justify-center flex-col ">
                         <a href="/my-courses">
-                            <div class="w-full flex items-center p-3 rounded-2xl">
+                            <div class="w-10 flex items-center px-2 py-1 rounded-2xl mx-auto">
                                 <x-svg.navicons.layout class="stroke-white" />
                             </div>
                         </a>
                     </li>
-                    <li class="flex justify-center flex-col my-3">
+                    <li class="flex justify-center flex-col">
                         <a href="/new-courses">
-                            <div class="w-full flex items-center p-3 rounded-2xl">
+                            <div class="w-10 flex items-center px-2 py-1 rounded-2xl mx-auto">
                                 <x-svg.navicons.layout-add class="stroke-white" />
                             </div>
                         </a>
                     </li>
-                    <li class="flex justify-center flex-col my-3">
+                    <li class="flex justify-center flex-col">
                         <a href="/expert/list">
-                            <div class="w-full flex items-center p-3 rounded-2xl">
+                            <div class="w-10 flex items-center px-2 py-1 rounded-2xl mx-auto">
                                 <x-svg.navicons.teach class="stroke-white" />
                             </div>
                         </a>
                     </li>
-                    <li class="flex justify-center flex-col my-3">
+                    <li class="flex justify-center flex-col">
                         <a href="/competention-profile">
-                            <div class="w-full flex items-center p-3 rounded-2xl">
+                            <div class="w-10 flex items-center px-2 py-1 rounded-2xl mx-auto">
                                 <x-svg.navicons.radar class="stroke-white" />
                             </div>
                         </a>
                     </li>
-                    <li class="flex justify-center flex-col my-3">
+                    <li class="flex justify-center flex-col">
                         <a href="/certificates">
-                            <div class="w-full flex items-center p-3 rounded-2xl">
+                            <div class="w-10 flex items-center px-2 py-1 rounded-2xl  mx-auto">
                                 <x-svg.navicons.certificate class="stroke-white" />
                             </div>
                         </a>
@@ -57,20 +58,13 @@
                 </ul>
             </div>
             <div>
-                <ul class="mx-5">
-                    <li class="flex justify-center flex-col my-3">
-                        <a href="/settings">
-                            <div class="w-full flex items-center p-3 rounded-2xl">
-                                <x-svg.navicons.settings class="stroke-white" />
-                            </div>
-                        </a>
-                    </li>
-                    <li class="flex justify-center flex-col my-3">
+                <ul class="mx-3">
+                    <li class="flex justify-center flex-col my-2">
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
                             <a class="cursor-pointer" :href="route('logout')" onclick="event.preventDefault();
                                                         this.closest('form').submit();">
-                                <div class="w-full flex items-center p-3 rounded-2xl">
+                                <div class="w-10 flex items-center px-2 py-1 rounded-2xl mx-auto">
                                     <x-svg.navicons.logout class="stroke-red-600" />
                                 </div>
                             </a>
