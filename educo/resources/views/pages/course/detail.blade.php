d3033d56d1222e22ca72fe3bc8b66745f8599ab3
Ruben Rooms <ruben.rooms@endare.com>
2022-06-12 18:16:30 +0200

    styled detail page of course (partially)

--- educo/resources/views/pages/course/detail.blade.php
+++ educo/resources/views/pages/course/detail.blade.php
@@ -1,31 +1,64 @@
 <x-app-layout>
-    <section>
-        <h1>{{$course->title}}</h1>
-        <h3><span>Created by: </span>{{$expert->name}}</h3>
-        <h3 class="text-tertiary font-bold text-2xl mt-3">Skills</h3>
-        @foreach($skills as $key => $skill)
-            <div class="inline-block bg-gray-200 px-2 rounded my-2 mr-2 shadow-md">{{$skill->title}}</div>
-        @endforeach
-        @if(!$participation)
-            <div>
-                <p>{{$course->description}}</p>
-                <h2>Course structure</h2>
+    <section class="container mx-auto mt-6">
+        <h1 class="text-primary font-bold text-3xl mt-3">{{$course->title}}</h1>
+        <section class=" ml-12">
+        <div class="grid grid-cols-2 mt-6">
+            <div class="">
+                <div class="w-3/5">
+                    <h3 class="text-primary font-bold text-lg mt-4">Description</h3>
+                    <p class="mt-3">{{$course->description}}</p>
+                </div>
                 <div>
-                    @foreach($chapters as $chapter)
-                        <h3>{{$chapter->title}}</h3>
-                        @foreach($chapter->elements as $element)
-                            <p>{{$element->title}}</p>
-                        @endforeach
+                    <h3 class="text-primary font-bold text-lg mt-4">Skills</h3>
+                    @foreach($skills as $key => $skill)
+                        <div class="inline-block bg-white px-2 rounded my-2 mr-2 shadow-md">{{$skill->title}}</div>
                     @endforeach
                 </div>
+            </div>
+            <div class="flex justify-center">   
+                <div class=" w-44">
+                        <h3 class="mb-2 text-primary text-lg font-bold text-center">Created by</h3>
+                    <div class="bg-white p-6 rounded-md my-2 mr-2 shadow-md">
+                        <img class="rounded-full w-32 mx-auto" src="{{ asset('storage/' . $expert->picture) }}" alt="expert_picture">
+                        <h3 class="mt-3 text-gray-700 font-semibold text-center">{{$expert->name}}</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-                <a href="{{route('course.participation', $course->id)}}">
-                    start this course
+        
+
+        @if(!$participation)
+        <h2 class="text-primary font-bold text-xl mt-4">Course structure</h2>
+        <div class="">
+            <div class="mt-3">
+                @foreach($courseAttr->chapters as $chapter)
+                <div class="bg-white p-6 rounded-md my-2 mr-2 shadow-md">
+                    <h3 class=""><span class="font-bold pt-2 pb-2">Chapter {{$chapter->order}} - </span>{{$chapter->title}}</h3>
+                    @foreach($chapter->elements as $element)
+                        @if($element->type === "video")
+                        <div class="bg-grey-200">
+                            <p class="px-6 py-1 rounded-md"><span class="font-semibold ">Video - </span>{{$element->title}}</p>
+                        </div>
+                        @else
+                        <div class="bg-grey-200">
+                            <p class="px-6 py-1 rounded-md"><span class="font-semibold">Task - </span>{{$element->title}}</p>
+                        </div>
+                        @endif
+                    @endforeach
+                    </div>
+                @endforeach
+            </div>
+            <div class="flex justify-center">
+                <a  href="{{route('course.participation', $course->id)}}">
+                    <button class="bg-secondary rounded-md my-2 mr-2 shadow-md text-white p-4">start this course</button>
                 </a>
             </div>
+
+        </div>
         @else
 
-            <div>
+            <div class="bg-white p-6 rounded-md my-2 mr-2 shadow-md">
                 <h2><span>Current chapter: </span>{{$activeChapter->title}}</h2>
                 <h3><span>Current: </span>{{$activeElement->title}}</h3>
                 <p>{{$activeElement->description}}</p>
@@ -66,6 +99,8 @@
                 <a href="{{route('next.step.course', [$activeElement->id, $activeChapter->id])}}">next step</a>
             </div>
         @endif
+        </section>
+        
     </section>
 
     <script>
