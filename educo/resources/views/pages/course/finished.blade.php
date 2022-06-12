e5ba44eea4ad1ce5c6edad51c7dc7e8596858566
victormassaer <victor@massaer.be>
2022-06-12 10:58:56 +0200

    course finished page styling

--- educo/resources/views/pages/course/finished.blade.php
+++ educo/resources/views/pages/course/finished.blade.php
@@ -1,14 +1,20 @@
 <x-app-layout>
-    <h1>Course Finished, congratulations!</h1>
-    <h2>{{$course->title}}</h2>
-    <div>
-        <h2>Certificates obtained</h2>
-        @foreach($certificates as $certificate)
-            <div>
-                <h2>{{$certificate->title}}</h2>
-                <p>{{$certificate->date_acquired}}</p>
-            </div>
-        @endforeach
-    </div>
-
+    <section class="px-44">
+        <h1 class="font-bold text-primary text-2xl">Course Finished, congratulations!</h1>
+        <h2 class="font-bold text-tertiary text-xl">{{$course->title}}</h2>
+        <div class="my-5">
+            <h2 class="font-bold text-primary text-xl">Certificates obtained</h2>
+            @foreach($certificates as $certificate)
+                <div class="inline-block mr-5 mt-4 text-center bg-white rounded p-6 shadow-md">
+                    <img src="{{asset('images/logo2color.png')}}" class="rounded w-24 border-solid border-8 p-2 border-secondary" alt="certicate image">
+                    <p class="font-bold">{{$certificate->title}}</p>
+                    <span>Acquired at: </span>
+                    <p>{{\Carbon\Carbon::parse($certificate->date_acquired)->format('j F, Y')}}</p>
+                </div>
+            @endforeach
+        </div>
+        <div>
+            <a class="px-3 py-2 bg-tertiary rounded text-white font-bold" href="{{route('dashboard')}}">back to dashboard</a>
+        </div>
+    </section>
 </x-app-layout>
--- educo/resources/views/pages/user/dashboard.blade.php
+++ educo/resources/views/pages/user/dashboard.blade.php
@@ -1,15 +1,15 @@
 <x-app-layout>
-        <div class="py-3 w-3/5 max-w-3xl mx-auto flex flex-row justify-around">
-            <a href="/dashboard"><p class="font-semibold">All courses</p></a>
-            <a href="{{route('dashboard.active', $user->id)}}"><p>Active</p></a>
-            <a href="{{route('dashboard.obligated', $user->id)}}"><p>Obligated</p></a>
-            <a href="{{route('dashboard.finished', $user->id)}}"><p>Finished</p></a>
-            <a href="{{route('dashboard.recommended', $user->id)}}"><p>Recommended</p></a>
-        </div>
-        <div class="py-5 w-5/6 max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center flex-wrap">
-            @foreach($courses as $course)
-                <x-course :course="$course" class=""/>
-            @endforeach
-        </div>
-        {{ $courses->onEachSide(4) }}
+    <div class="py-3 w-3/5 max-w-3xl mx-auto flex flex-row justify-around">
+        <a href="/dashboard"><p class="font-semibold">All courses</p></a>
+        <a href="{{route('dashboard.active', $user->id)}}"><p>Active</p></a>
+        <a href="{{route('dashboard.obligated', $user->id)}}"><p>Obligated</p></a>
+        <a href="{{route('dashboard.finished', $user->id)}}"><p>Finished</p></a>
+        <a href="{{route('dashboard.recommended', $user->id)}}"><p>Recommended</p></a>
+    </div>
+    <div class="py-5 w-5/6 max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center flex-wrap">
+        @foreach($courses as $course)
+            <x-course :course="$course" class=""/>
+        @endforeach
+    </div>
+    {{ $courses->onEachSide(4) }}
 </x-app-layout>
